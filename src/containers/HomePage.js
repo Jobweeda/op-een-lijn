4778e4b71f6edd5aef3389ca54e7f1b88c86d50d
jobweeda <jobweeda@hotmail.com>
2017-10-31 17:15:28 +0100

    adds a homepage + routes

---
+++ src/containers/HomePage.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react';
+
+class HomePage extends PureComponent {
+
+  render() {
+    return(
+      <div className='home-page'>
+        <div className='row'>
+        <h1> Patienten </h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default HomePage
--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute } from 'react-router';
+import store, { history } from './store';
+import HomePage from './containers/HomePage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+  <Router history={history}>
+     <Route path="/" component={App}>
+       <IndexRoute component={HomePage} />
+     </Route>
+   </Router>
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker();
